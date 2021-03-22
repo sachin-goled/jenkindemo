a83cb7609c23c51d0954a734d207ea86b437e47e
sachin-goled <80738026+sachin-goled@users.noreply.github.com>
2021-03-22 11:09:36 +0530

    updated by developer

--- test.bat
+++ test.bat
@@ -1,7 +1,8 @@
 echo off
 echo "creating a folder"
-md c:\jenkindemo1
-systeminfo >> c:\jenkindemo1\capture.txt
+md c:\jenkindemo2
+systeminfo >> c:\jenkindemo2\capture.txt
+ipconfig >> c:\jenkindemo2\myip.txt
 echo "inventory is getting captured *********"
 echo "inventory is captured"
 
